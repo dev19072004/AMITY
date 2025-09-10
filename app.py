78f7d1c0a113d88d4b4cbca36d5b43b9ef8d6f6a
dev19072004 <devanshuverma72@gmail.com>
2025-09-10 11:14:13 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 #importing the libraries
 import pandas as pd
 import numpy as np
-import seaborn as sns
+# import seaborn as sns
 import matplotlib.pyplot as plt
 
 from sklearn.cluster import KMeans
