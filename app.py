6a0903369527a8a8f8271037bfdbd3d57e778ce8
dev19072004 <devanshuverma72@gmail.com>
2025-09-10 11:13:08 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 #importing the libraries
 import pandas as pd
-# import numpy as np
-# import seaborn as sns
-# import matplotlib.pyplot as plt
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
 
 from sklearn.cluster import KMeans
 from sklearn.preprocessing import StandardScaler
